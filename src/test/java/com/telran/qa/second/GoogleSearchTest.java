c3fed794e2e2e6c93eb5b06c3e037bef0f5958a7
Lena <etarnovskaya@gmail.com>
2021-03-25 21:31:54 +0200

    created methods type and click

--- src/test/java/com/telran/qa/second/GoogleSearchTest.java
+++ src/test/java/com/telran/qa/second/GoogleSearchTest.java
@@ -3,28 +3,66 @@ package com.telran.qa.second;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
 public class GoogleSearchTest {
     WebDriver wd;
 
-    @Test
-    public void testGoogleSearch(){
+    @BeforeClass
+    public void setUp() {
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
+        wd.manage().window().maximize();
         wd.get("https://www.google.com/");
+    }
 
+    @Test
+    public void testGoogleSearch() {
+        type(By.name("q"), "WebDriver" + Keys.ENTER);
+        click(By.cssSelector("[href='https://www.selenium.dev/documentation/en/webdriver/']"));
+        back();
+        //Assert
+    }
 
-        wd.findElement(By.name("q")).click();
-        wd.findElement(By.name("q")).clear();
-        wd.findElement(By.name("q")).sendKeys("WebDriver" + Keys.ENTER);
+    @Test
+    public void testSearchImg() {
+        type(By.name("q"), "Java" + Keys.ENTER);
+        click(By.cssSelector("[data-hveid='CAEQAw']"));
+    }
 
-        wd.findElement(By.cssSelector("[href='https://www.selenium.dev/documentation/en/webdriver/']")).click();
+    public int multy(int a, int b){
+        return a*b;
+    }
+
+    @Test(enabled = false)
+    public void myTest(){
+        int res = multy(2,5);
+        System.out.println(res);
+        Assert.assertEquals(res, 10);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        wd.quit();
+    }
+
+    //_____________________________________________
+    private void back() {
+        wd.navigate().back();
+    }
+
+    public void type(By locator, String text) {
+        click(locator);
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+    }
+
+    public void click(By locator){
+        wd.findElement(locator).click();
     }
 
 }
